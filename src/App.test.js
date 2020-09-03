399ca1c86c8cc85e5aad3e83960e3cb8b8384252
David Johnson <davidjohnson@davids-mac-book.local>
2020-09-03 13:28:44 +0100

    Added basic application info

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText('React Library Store');
+
   expect(linkElement).toBeInTheDocument();
 });
