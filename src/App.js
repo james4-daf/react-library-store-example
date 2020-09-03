54ff417f7dbb8239263b2b97127f03efe7d4b0a7
David Johnson <davidjohnson@davids-mac-book.local>
2020-09-03 13:34:43 +0100

    Added first step files

--- src/App.js
+++ src/App.js
@@ -8,10 +8,41 @@ function App() {
         <h1>React Library Store</h1>
       </header>
       <main>
-
+        <div>
+          <h3>Books</h3>
+          <ul>
+            <li>
+              <div>
+                <div>The colour of magic</div>
+                <div>Terry Pratchett</div>
+                <div>Fantasy</div>
+              </div>
+            </li>
+            <li>
+              <div>
+                <div>The Light Fantastic</div>
+                <div>Terry Pratchett</div>
+                <div>Fantasy</div>
+              </div>
+            </li>
+            <li>
+              <div>
+                <div>Death de Jour</div>
+                <div>Kathy Reichs</div>
+                <div>Crime</div>
+              </div>
+            </li>
+            <li>
+              <div>
+                <div>The Holy Bible</div>
+                <div>Various</div>
+                <div>Religion</div>
+              </div>
+            </li>
+          </ul>
+        </div>
       </main>
       <footer>
-
       </footer>
     </div>
   );
